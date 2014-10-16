368b7d4dfaf3ee0fd804f8995cc83bc287888dde
dreamsea <amegafuru@gmail.com>
2014-10-16 15:15:35 -0500

    Fixed LoadDictionary
    test_LoadDictionary.asm: Strange things were happening when there were
    both carriage returns and line feeds. Now ignores CR when found and
    starts new word on LF. Also cleaned up a few jump/branches.

--- test_link/test_LoadDictionary.asm
+++ test_link/test_LoadDictionary.asm
@@ -1,6 +1,12 @@
+############################################################
+# Notes:
+# - dict .space must be 10 * number of words/lines in .txt
+# - the mars.jar must be located in the same location as .txt
+############################################################
+
 .data
 	buffer:	.space 10	# arbitrary
-	dict:	.space 70	# number of words * 10 bytes (max length 9 letter + null)
+	dict:	.space 90	# number of words * 10 bytes (max length 9 letter + null)
 	file:	.asciiz "words.txt"
 .text
 
@@ -28,32 +34,34 @@ loadDict:
  	li	$a2, 10		# length of buffer (change this if changing buffer size)
  	la	$t1, dict	# save address of dictionary
  	li	$t4, 10		# (init) number of bytes allowed per word in dictionary
- readMore:
+readMore:
  	la	$a1, buffer	# reset buffer address
  	li	$v0, 14		# syscall read file
  	syscall
  	beqz	$v0, finishReading	# no more letters to read
- 	j	scanBuffer		# jump to buffer iteration
- nextWord:
- 	beqz	$v0, readMore		# read more from file if no letters left in buffer
- 	add	$t1, $t1, $t4		# start next word on a multiple of 10
- 	li	$t4, 10			# (refresh) number of bytes/word
- scanBuffer:
+scanBuffer:
+ 	beqz	$v0, readMore		# check if theres anything left in buffer
  	lb	$t2, 0($a1)		# load byte at current index of buffer
- 	beqz	$t2, finishReading	# null term found, no more to read
- 	blt	$t2, 48, newlineFound	# newline found, prep for next word
+ 	#beqz	$t2, finishReading	# null term found, no more to read
+ 	beq	$t2, 13, carriageFound	# (13) CR
+ 	beq	$t2, 10, newlineFound	# (10) NL
  	sb	$t2, 0($t1)		# store byte in current index of dictionary
  	subi	$v0, $v0, 1		# decr number of bytes left to read in buffer
  	subi	$t4, $t4, 1		# decr number of spaces needed to align word
  	addi	$a1, $a1, 1		# incr buffer index
  	addi	$t1, $t1, 1		# incr dict index
- 	bnez	$v0, scanBuffer		# iterate through buffer if not at end
- 	j readMore
- newlineFound:
+ 	j 	scanBuffer
+carriageFound:			# ignores carriage returns
+ 	addi	$a1, $a1, 1	# incr index past carriage
+ 	subi	$v0, $v0, 1	# decr bytes left to read
+ 	j	scanBuffer
+newlineFound:			# prep for next word
  	addi	$a1, $a1, 1	# incr index past newline
  	subi	$v0, $v0, 1	# decr bytes left to read
- 	j nextWord
- finishReading:
+	add	$t1, $t1, $t4	# start next word on a multiple of 10
+ 	li	$t4, 10		# (refresh) number of bytes/word
+ 	j	scanBuffer
+finishReading:
  	li	$v0, 16		# close file
  	syscall
  	la	$v0, dict	# load dictionary address in $v0 to return
